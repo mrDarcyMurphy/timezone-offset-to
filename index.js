9d0cf889800c346f0c36739ca149f5ff34b1c87b
Darcy Murphy <mrDarcyMurphy@gmail.com>
2013-06-06 13:16:21 -0500

    wrap it in boilerplate

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
-var parse = function(offset) {
-  offset = offset || ""
-  var tz = 0
-  var tzRX = /(-?)(\d{1,2}):??(\d{2})/
-  var match = offset.match(tzRX)
-  if (match) tz = parseInt(match.slice(1,4).join(''), 10)
-  return tz
-}
+!function (name, definition) {
+  if (typeof module != 'undefined') module.exports = definition()
+  else if (typeof define == 'function' && typeof define.amd == 'object') define(definition)
+  else this[name] = definition()
+}('tzto', function () {
 
-var tzTo = module.exports = {
-
-  minutes: function(offset) {
-    var tz = parse(offset)
-    return (((tz / 100) | 0) * 60) + tz % 100
-  },
-
-  hours: function(offset) {
-    var tz = parse(offset)
-    return ((tz / 100) | 0) + ((tz % 100) / 60)
+  var parse = function(offset) {
+    offset = offset || ""
+    var tz = 0
+    var tzRX = /(-?)(\d{1,2}):??(\d{2})/
+    var match = offset.match(tzRX)
+    if (match) tz = parseInt(match.slice(1,4).join(''), 10)
+    return tz
   }
 
-}
+  var tzto = {
+
+    minutes: function(offset) {
+      var tz = parse(offset)
+      return (((tz / 100) | 0) * 60) + tz % 100
+    },
+
+    hours: function(offset) {
+      var tz = parse(offset)
+      return ((tz / 100) | 0) + ((tz % 100) / 60)
+    }
+
+  }
+
+  return tzto
+
+})
